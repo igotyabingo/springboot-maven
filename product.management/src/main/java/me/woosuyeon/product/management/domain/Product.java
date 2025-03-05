72c87e4c0e888bd563399af62540b3e2a225c14c
igotyabingo <jenifer7933@snu.ac.kr>
2025-03-05 12:18:44 +0900

    상품 관리 애플리케이션 chatper 9 실습 완료

---
+++ product.management/src/main/java/me/woosuyeon/product/management/domain/Product.java
@@ -0,0 +1,33 @@
+package me.woosuyeon.product.management.domain;
+
+import me.woosuyeon.product.management.presentation.ProductDto;
+
+import java.util.Objects;
+
+public class Product {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer amount;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean sameId(Long id) {
+        return this.id.equals(id);
+    }
+
+    public Boolean containsName(String name) {
+        return this.name.contains(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+}
---
+++ product.management/src/main/java/me/woosuyeon/product/management/presentation/ProductController.java
@@ -0,0 +1,47 @@
+package me.woosuyeon.product.management.presentation;
+
+import me.woosuyeon.product.management.application.SimpleProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private SimpleProductService simpleProductService;
+
+    @Autowired
+    ProductController(SimpleProductService simpleProductService) {
+        this.simpleProductService = simpleProductService;
+    }
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+        return simpleProductService.add(productDto);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    public ProductDto findProductById(@PathVariable Long id) {
+        return simpleProductService.findById(id);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public List<ProductDto> findProducts(@RequestParam(required = false) String name) {
+        // required = false 이므로 null일떄의 처리도 해주어야 한다.
+        if (null == name)
+            return simpleProductService.findAll();
+
+
+        return simpleProductService.findByNameContaining(name);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ProductDto updateProduct(@PathVariable Long id, @RequestBody ProductDto productDto) {
+        productDto.setId(id);
+        return simpleProductService.update(productDto);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public void deleteProduct(@PathVariable Long id) {
+        simpleProductService.delete(id);
+    }
+}
