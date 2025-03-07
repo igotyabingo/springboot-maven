6ebfee588a246935b219f4000c5c86ed25f05e5c
igotyabingo <jenifer7933@snu.ac.kr>
2025-03-07 14:00:49 +0900

    chapter 11 mySQL DB 연동

---
+++ product.management/src/main/java/me/woosuyeon/product/management/infrastructure/DatabaseProductRepository.java
@@ -0,0 +1,85 @@
+package me.woosuyeon.product.management.infrastructure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import me.woosuyeon.product.management.domain.Product;
+
+import java.util.Collections;
+import java.util.List;
+
+@Repository
+public class DatabaseProductRepository {
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    @Autowired
+    public DatabaseProductRepository (NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+    public Product add(Product product) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource namedParameter = new BeanPropertySqlParameterSource(product);
+        namedParameterJdbcTemplate
+                .update("INSERT INTO PRODUCTS (name, price, amount) VALUES (:name, :price, :amount)",
+                        namedParameter, keyHolder);
+
+        Long generatedId = keyHolder.getKey().longValue();
+        product.setId(generatedId);
+
+        return product;
+    }
+
+    public Product findById(Long id) {
+        SqlParameterSource namedParameter = new MapSqlParameterSource("id", id);
+        Product product = namedParameterJdbcTemplate.queryForObject(
+                "SELECT id, name, price, amount FROM PRODUCTS WHERE id=:id",
+                namedParameter,
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return product;
+    }
+
+    public List<Product> findAll() {
+        List<Product> products = namedParameterJdbcTemplate.query(
+                "SELECT * FROM PRODUCTS",
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return products;
+    }
+
+    public List<Product> findByNameContaining(String name) {
+        SqlParameterSource namedParameter = new MapSqlParameterSource("name", "%"+name+"%");
+
+        List<Product> products = namedParameterJdbcTemplate.query(
+                "SELECT * FROM PRODUCTS WHERE name LIKE :name",
+                namedParameter,
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return products;
+    }
+
+    public Product update(Product product) {
+        SqlParameterSource namedParameter = new BeanPropertySqlParameterSource(product);
+        namedParameterJdbcTemplate
+                .update("UPDATE PRODUCTS SET name=:name, price=:price, amount=:amount WHERE id=:id",
+                        namedParameter);
+
+        return product;
+    }
+
+    public void delete(Long id) {
+        SqlParameterSource namedParameter = new MapSqlParameterSource("id", id);
+        namedParameterJdbcTemplate
+                .update("DELETE FROM PRODUCTS WHERE id=:id",
+                        namedParameter);
+    }
+
+}
