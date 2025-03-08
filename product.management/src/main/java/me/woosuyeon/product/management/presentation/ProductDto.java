1fde450336281ed9acd79a93389bc1297d0004a9
igotyabingo <jenifer7933@snu.ac.kr>
2025-03-08 13:53:40 +0900

    chapter 12 SimpleProductService 통합 테스트

--- product.management/src/main/java/me/woosuyeon/product/management/presentation/ProductDto.java
+++ product.management/src/main/java/me/woosuyeon/product/management/presentation/ProductDto.java
@@ -1,6 +1,7 @@
 package me.woosuyeon.product.management.presentation;
 
 import jakarta.validation.constraints.NotNull;
+import me.woosuyeon.product.management.domain.Product;
 
 public class ProductDto {
     private Long id;
@@ -10,6 +11,20 @@ public class ProductDto {
     private Integer price;
     @NotNull
     private Integer amount;
+
+    public ProductDto() {
+    }
+    public ProductDto(String name, Integer price, Integer amount) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+    public ProductDto(Long id, String name, Integer price, Integer amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
 
     public Long getId() {
         return id;
@@ -31,4 +46,18 @@ public class ProductDto {
         return amount;
     }
 
+    public static Product toEntity(ProductDto productDto) {
+        Product product = new Product(
+                productDto.getId(), productDto.getName(), productDto.getPrice(), productDto.getAmount()
+        );
+
+        return product;
+    }
+
+    public static ProductDto toDto(Product product) {
+        ProductDto productDto = new ProductDto(
+                product.getId(), product.getName(), product.getPrice(), product.getAmount());
+
+        return productDto;
+    }
 }
