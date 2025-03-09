3fd5923f6c7eb7faf61c80ab3e7fa7eb92a939f5
igotyabingo <jenifer7933@snu.ac.kr>
2025-03-09 14:18:31 +0900

    프론트 페이지 추가 및 static 접근 경로 수정

--- shorten-url-service/src/main/java/me/woosuyeon/shorten/url/service/presentation/UrlApiController.java
+++ shorten-url-service/src/main/java/me/woosuyeon/shorten/url/service/presentation/UrlApiController.java
@@ -2,8 +2,13 @@ package me.woosuyeon.shorten.url.service.presentation;
 
 import me.woosuyeon.shorten.url.service.application.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 public class UrlApiController {
@@ -18,6 +23,18 @@ public class UrlApiController {
     public ResponseEntity<ShortenUrlDto> createNewKey(@RequestBody ShortenUrlDto shortenUrlDto) {
         ShortenUrlDto savedDto = urlService.createNewKey(shortenUrlDto);
         return ResponseEntity.ok().body(savedDto);
+    }
+
+    // 2. 단축 URL 리다이렉트 API
+    @RequestMapping(value = "/{key}", method = RequestMethod.GET)
+    public ResponseEntity<?> redirectToOriginalUrl(@PathVariable String key) throws URISyntaxException{
+        String redirectTo = urlService.redirectToOriginalUrl(key);
+        // 저장할 때 부터 절대 경로를 입력해야 함 www.naver.com 이 아니라 https://www.naver.com
+        URI redirectUri = new URI(redirectTo);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(redirectUri);
+
+        return new ResponseEntity<>(httpHeaders, HttpStatus.MOVED_PERMANENTLY);
     }
 
     // 3. 단축 URL 정보 조회 API
